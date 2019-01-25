320945b072076e18217c5918c92618c84e3ddc2e
Chuck D'Antonio <c_dantonio@acm.org>
2019-01-25 15:33:47 -0500

    Update site info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    author: `Chuck D'Antonio`,
+    description: `The place a blog will be.`,
+    siteUrl: `https://crdant.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `crdant`,
     },
   },
   plugins: [
