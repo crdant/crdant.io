fdf509c88ef5be4b790d0d6d98f03399429fb470
Chuck D'Antonio <cdantonio@pivotal.io>
2020-02-11 15:20:18 -0500

    Replace portfolio naming with speaking

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ export class Navbar extends Component {
         >
           <Link to={'/'}>Home</Link>
           <Link to={'/journey'}>Journey</Link>
-          <Link to={'/portfolio'}>Speaking</Link>
+          <Link to={'/speaking'}>Speaking</Link>
           <Link to={'/blog'}>Blog</Link> 
           <Link to={'/resume'}>Resume</Link>
           <Link to={'/contact'}>Contact</Link>
