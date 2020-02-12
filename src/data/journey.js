9190af8bf16f0a0418977f9ceab0bd23442c55fe
Chuck D'Antonio <cdantonio@pivotal.io>
2020-02-12 12:29:33 -0500

    Start with my own items

--- src/data/journey.js
+++ src/data/journey.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaAward, FaPaperPlane } from 'react-icons/fa'
+import { MdComputer  } from 'react-icons/md'
 
 import Image1 from './../assets/images/image1.jpg'
 import Image2 from './../assets/images/image2.jpg'
@@ -7,33 +7,13 @@ import Image3 from './../assets/images/image3.jpg'
 
 const data = [
   {
-    date: 'November 15th 2012',
-    headline: 'Milestone 1',
-    description: `ulputate sapien nec sagittis aliquam
-    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
-    ligula ullamcorper.`,
+    date: 'February 1993',
+    headline: 'Selected a Path',
+    description: `Realized a career in journalism with programming as
+    a hobby might not be the best way to go about things.`,
     image: Image1,
-    icon: <FaAward />,
-  },
-  {
-    date: 'November 26th 2011',
-    headline: 'Milestone 2',
-    description: `ulputate sapien nec sagittis aliquam
-    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
-    ligula ullamcorper.`,
-    image: Image2,
-    icon: <FaPaperPlane />,
-  },
-
-  {
-    date: 'June 6th 2011',
-    headline: 'Milestone 3',
-    description: `ulputate sapien nec sagittis aliquam
-    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
-    ligula ullamcorper.`,
-    image: Image3,
-    icon: <FaPaperPlane />,
-  },
+    icon: <MdComputer />,
+  }
 ]
 
 export default data
