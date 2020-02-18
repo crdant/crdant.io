afa34f6b19dec8f23d4d5efa2ae7b2b8a6f9fb0d
Chuck D'Antonio <cdantonio@pivotal.io>
2020-02-18 17:32:09 -0500

    Drop in different text (from Twitter bio)

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,19 +21,10 @@ class SiteIndex extends React.Component {
           <meta name="description" content={siteDescription} />
         </Hemlet>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue
-          eget arcu dictum varius duis. Vestibulum lectus mauris ultrices eros
-          in cursus turpis. Blandit volutpat maecenas volutpat blandit aliquam
-          etiam. Sagittis purus sit amet volutpat. Nullam ac tortor vitae purus
-          faucibus ornare suspendisse. Vulputate sapien nec sagittis aliquam
-          malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
-          ligula ullamcorper. Sit amet cursus sit amet dictum sit amet.
-          Malesuada proin libero nunc consequat interdum varius sit amet. Sed
-          egestas egestas fringilla phasellus faucibus scelerisque eleifend.
-          Habitasse platea dictumst quisque sagittis. Sem viverra aliquet eget
-          sit amet tellus cras adipiscing enim. Amet massa vitae tortor
-          condimentum lacinia quis vel eros donec.
+          A zealous customer advocate and
+          father to 2(+1). Chuck integrates work as system architect, servant leader, and
+          trusted advisor with a passion for cooking, composing cocktails, competitive
+          open water swimming.
         </p>
         <img src={DisplayImage} alt={siteTitle} />
       </Layout>
