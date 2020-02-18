130542aa09b13765040a4606dabd7ab357c4b9bb
Chuck D'Antonio <cdantonio@pivotal.io>
2020-02-18 14:16:04 -0500

    Start adjusting styles with Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h2 
-        style={{
-          textAlign: 'center',
-          fontFamily: 'Luckiest Guy',
-          fontSize: '5vw'
-        }}
-      >Chuck D'Antonio</h2> 
+      <h2 className='site-title'>
+        Chuck D'Antonio
+      </h2> 
     </div>
   )
 }
