f02ec72838c3bed70b1b22b80bbf9c0d73c958d5
Chuck D'Antonio <cdantonio@pivotal.io>
2020-02-19 11:47:44 -0500

    Remove typography from blog bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,27 +6,15 @@ import 'typeface-merriweather'
 
 import Gravatar from 'react-gravatar'
 
-import { rhythm } from '../utils/typography'
 import bioData from './../data/bio'
 
 class Bio extends React.Component {
   render() {
     return (
-      <div className="blog-bio"
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
-        }}
-      >
+      <div className="blog-bio">
         <Gravatar
           email='chuck@thedantonios.net'
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-            borderRadius: "50%"
-          }}
+          className="avatar"
         />
         <p>{bioData}</p>
       </div>
